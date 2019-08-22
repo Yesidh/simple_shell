59c656a60753d436fec5615f77a6cb4c0dfb66c0
Yesidh <944@holbertonschool.com>
2019-08-22 17:44:13 -0500

    #Feature: prototype file holberton.h

---
+++ holberton.h
@@ -0,0 +1,13 @@
+#ifndef SIMPLESHELL
+#define SIMPLESHELL
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+char *_getenv(const char *name);
+#endif
