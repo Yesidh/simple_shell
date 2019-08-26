447763f0b251971b379f911774092ad7bdc0fa3a
Yesidh <944@holbertonschool.com>
2019-08-26 13:51:55 -0500

    #Fix: adding malloc and stat function into the file concatenatokens.c

--- concatenatokens.c
+++ concatenatokens.c
@@ -5,18 +5,27 @@
  * @tokenpath: pointer to tokens of the PATH
  * Return: a pointer to concatenate string
  */
-char *concatenatokens(char **tokencommand, char **tokenpath)
+char *concatenatokens(char **tokenscommand, char **tokenspath)
 {
-	unsigned int i, j;
-	char *stringconcat = { 0 };
+	size_t j = 0, k = 0, l = 0;
+	char *stringconcat;
+	struct stat st;
 
-	for (; tokenpath != NULL;)
+	for (j = 0; tokenspath[j] != NULL; j++)
 	{
-		for (i = 1; tokenpath[i - 1] != NULL; i++)
-			stringconcat[i] = tokenpath[i];
-		stringconcat[i] = '/';
-		for (j =1; tokencommand[j - 1] != NULL; j++, i++)
-			stringconcat[i] = tokencommand[j];
+		stringconcat = malloc(sizeof(char) * 120);
+
+		for (k = 0; tokenspath[j][k] != '\0'; k++)
+			stringconcat[k] = tokenspath[j][k];
+		stringconcat[k] = '/';
+		k++;
+		for (l = 0; tokenscommand[0][l] != '\0'; l++, k++)
+			stringconcat[k] = tokenscommand[0][l];
+		stringconcat[k] = '\0';
+		printf("concatenado es: %s\n", stringconcat);
+		if (stat(stringconcat, &st) == 0)
+			return(stringconcat);
+		free(stringconcat);
 	}
-	return(stringconcat);
+	return(0);
 }
