448df525885173ed39768c83e8539e5c349a7261
Yesidh <944@holbertonschool.com>
2019-08-26 09:37:19 -0500

    #Fix: using malloc in double pointer

--- tokenize.c
+++ tokenize.c
@@ -9,24 +9,68 @@
 char **tokenize(char *stringtotokens, const char *delim)
 {
 	char **tokens;
-	size_t i;
+	size_t i = 0, j = 0, k = 0, tok = 0;
+	size_t lenghstring = 0, flag = 0, lenghtoken = 0;
 
-	for (i = 0; stringtotokens[i]; i++)
-		;
-	tokens = malloc((i) * sizeof(char));
+	for (i = 0; stringtotokens[i] != '\0'; i++)
+	{
+		for (j = 0; delim[j] != '\0'; j++)
+		{
+			if (stringtotokens[i] == delim[j])
+				k++;
+		}
+	}
+	lenghstring = i;
+	tokens = (char **)malloc(sizeof(char *) * k);
 	if (tokens == NULL)
 	{
 		perror("Can't alloc memory in tokenize function");
 		exit(-1);
 	}
-	tokens[0] = strtok(stringtotokens, delim);
-	i = 1;
-	while (tokens[i])
+	for (i = 0; stringtotokens[i] != '\0'; i++)
 	{
-		tokens[i] = strtok(NULL, delim);
-		if (tokens[i] == '\0')
+		for (j = 0; delim[j] != '\0'; j++)
+ 		{
+			if (stringtotokens[i] == delim[j])
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if ( flag == 1)
 			break;
-		i++;
+	}
+	printf("caracteres del primer token: %ld", i);
+	lenghtoken = i;
+	tokens[0] = (char *)malloc(sizeof(char *) * i);
+	if (tokens[0] == NULL)
+	{
+		perror("cant' alloc memory into tokens");
+		exit(-1);
+	}
+	tokens[tok] = strtok(stringtotokens, delim);
+	while (*tokens)
+	{
+		flag = 0;
+		lenghtoken++;
+		tok++;
+		for (i = 0; stringtotokens[lenghtoken] != '\0'; i++, lenghtoken)
+		{
+			for (j = 0; delim[j] != '\0'; j++)
+			{
+				if (stringtotokens[lenghtoken] == delim[j])
+				{
+					flag = 1;
+					break;
+				}
+			}
+			if ( flag == 1)
+				break;
+		}
+		printf("tamanho para el malloc %ld\n", i);
+		tokens[tok] = strtok(NULL, delim);
+		if (tokens[tok] == NULL)
+			break;
 	}
 	return (tokens);
 }
