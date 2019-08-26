58aea29a3c2d11a12e4e748ed8afbcdae88b9a99
Yesidh <944@holbertonschool.com>
2019-08-26 14:53:35 -0500

    #Feature: function for execute a command worker.c

---
+++ worker.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * worker - funtion for execute a command
+ * @stringconcat: command directory
+ * @tokenscommand: arguments array command
+ *
+ * Return: always 1
+ */
+int worker(char *stringconcat, char **tokenscommand)
+{
+	pid_t pid, ppid, pidclosed, childpidstatus;
+
+	ppid = getppid();
+	pid = fork();
+	if (pid < 0)
+		perror("fork failure\n");
+	if (pid == 0)
+	{
+		execv(stringconcat, tokenscommand);
+		exit(5);
+	}
+	if (pid > 0)
+		pidclosed = wait(NULL);
+	return (0);
+}
