480a51dfce5be3feb1646bf6436060723688b623
Yesidh <944@holbertonschool.com>
2019-08-27 11:51:48 -0500

    #Fix: for valgrind

--- concatenatokens.c
+++ concatenatokens.c
@@ -8,7 +8,7 @@
 char *concatenatokens(char **tokenscommand, char **tokenspath)
 {
 	size_t j = 0, k = 0, l = 0;
-	char *stringconcat;
+	char *stringconcat = NULL;
 	struct stat st;
 
 	for (j = 0; tokenspath[j] != NULL; j++)
@@ -22,7 +22,6 @@ char *concatenatokens(char **tokenscommand, char **tokenspath)
 		for (l = 0; tokenscommand[0][l] != '\0'; l++, k++)
 			stringconcat[k] = tokenscommand[0][l];
 		stringconcat[k] = '\0';
-		printf("concatenado es: %s\n", stringconcat);
 		if (stat(stringconcat, &st) == 0)
 			return(stringconcat);
 		free(stringconcat);
