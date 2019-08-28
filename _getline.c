06bb1b7547fd60060eac115438d43acc3396356a
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-08-28 23:28:36 +0000

    betty for _getline.c

--- _getline.c
+++ _getline.c
@@ -23,36 +23,27 @@ char *_getline(void)
 			if (buf)
 				free(buf);
 			write(STDIN_FILENO, "\n", 1);
-			exit(0);
-		}
+			exit(0); }
 		if (buf[0] != '\n')
 		{
 			for (i = 0; buf[i] != '\0'; i++)
 			{
 				if (buf[i] == '\n')
-					return (buf);
-			}
-		}
+					return (buf); }}
 		else
 		{
-			continue;
-		}
-		signal(SIGINT, _sigint_handler);
+			continue; }
 		res = getline(&buf, &bufsize, stdin);
 		if (res == EOF)
 		{
 			if (buf)
 				free(buf);
 			write(STDIN_FILENO, "\n", 1);
-			exit(0);
-		}
-	}
+			exit(0); }}
 	if (res == EOF)
 	{
 		write(STDIN_FILENO, "\n", 1);
-		exit(0);
-	}
+		exit(0); }
 	if (buf)
 		free(buf);
-	return (NULL);
-}
+	return (NULL); }
