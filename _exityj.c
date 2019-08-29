3681443249d6715bb32b26adacb6c9459e342175
Jhonatan Galindo <jhonatan1988@gmail.com>
2019-08-29 16:20:09 +0000

    description for _exityj.c function

--- _exityj.c
+++ _exityj.c
@@ -1,7 +1,11 @@
 #include "holberton.h"
 /**
- * _exityj - exit the shell
- * @line: line write into the promp
+ * _exityj - exit the shell. this command is a basic implementation to exit
+ * the shell with a special command and not using ctrD or EOF. it does not
+ * finish the shell taking into account the status of the exit state.
+ *
+ * @line: line write into the prompt
+ *
  * Return: nothing
  */
 void _exityj(char **line)
